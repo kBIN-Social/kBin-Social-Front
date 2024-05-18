5c0331c11a6a94e68185e3895f768304329da03a
AleixFIB <aleix.lopez.padro@estudiantat.upc.edu>
2024-05-18 22:47:36 +0200

    ara els profiles fan les crides a per aconseguir el user

--- web/src/Pages/Profile.js
+++ web/src/Pages/Profile.js
@@ -5,12 +5,41 @@ import { useToken } from './TokenContext';
 
 function Profile() {
     const { token } = useToken();
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const userData = await getUserData(token);
+                setUser(userData);
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+            }
+        }
+        if (token) {
+            fetchUserData();
+        }
+    }, [token]);
+
+    async function getUserData(token) {
+        const response = await fetch(`https://asw-kbin.azurewebsites.net/api/v1/profile/me`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Token ${token}`,
+          },
+        });
+        if (!response.ok) {
+          throw new Error('Error fetching user data');
+        }
+        return response.json();
+    }
 
     return (
         <div>
             <body class="theme--dark" data-controller="kbin notifications" data-turbo="false">  
-                <ProfileHeader/>
-                <ProfileContent/>
+                <ProfileHeader user={user} />
+                <ProfileContent user={user} />
             </body>
         </div>
       );
--- web/src/Pages/ProfileEdit.js
+++ web/src/Pages/ProfileEdit.js
@@ -2,6 +2,37 @@ import ProfileForm from "../Components/ProfileForm";
 import ProfileHeader from "../Components/ProfileHeader";
 
 function ProfileEdit() {
+    const { token } = useToken();
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const userData = await getUserData(token);
+                setUser(userData);
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+            }
+        }
+        if (token) {
+            fetchUserData();
+        }
+    }, [token]);
+
+    async function getUserData(token) {
+        const response = await fetch(`https://asw-kbin.azurewebsites.net/api/v1/profile/me`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Token ${token}`,
+          },
+        });
+        if (!response.ok) {
+          throw new Error('Error fetching user data');
+        }
+        return response.json();
+    }
+    
     return (
         <div>
             <body class="theme--dark" data-controller="kbin notifications" data-turbo="false">
