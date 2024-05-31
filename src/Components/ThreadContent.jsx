99c1d83c07fb8505cb9f273a72c16e1109a05899
Pablo Fondevilla <pfondevi@gmail.com>
2024-05-31 13:43:53 +0200

    warnings 2

--- src/Components/ThreadContent.jsx
+++ src/Components/ThreadContent.jsx
@@ -1,17 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import { useToken, useUser } from '../Logic/UserContext';
+import { useToken} from '../Logic/UserContext';
+//import { useUser } from '../Logic/UserContext';
 import ThreadTemplate from './ThreadTemplate';
 import OptionBar from './OptionBar';
 
 function ThreadContent({ user }) {
   const token = useToken();
-  const authUser = useUser();
+  //const authUser = useUser();
   const [threadsData, setThreads] = useState([]);
   const [filterState, setFilterState] = useState('all');
   const [orderState, setOrderState] = useState('recent');
   const [forceUpdate, setForceUpdate] = useState(0);
 
   useEffect(() => {
+    async function getThreads(user, token) {
+      const response = await fetch(`https://asw-kbin.azurewebsites.net/api/v1/threads?order_by=${orderState}&filter_by=${filterState}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Token ${token}`,
+        },
+      });
+      if (!response.ok) {
+        throw new Error('Error fetching threads');
+      }
+      return response.json();
+    }
     const fetchData = async () => {
       try {
         const threadsData = await getThreads(user, token);
@@ -26,19 +40,6 @@ function ThreadContent({ user }) {
     }
   }, [user, token, filterState, orderState, forceUpdate]);
 
-  async function getThreads(user, token) {
-    const response = await fetch(`https://asw-kbin.azurewebsites.net/api/v1/threads?order_by=${orderState}&filter_by=${filterState}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Token ${token}`,
-      },
-    });
-    if (!response.ok) {
-      throw new Error('Error fetching threads');
-    }
-    return response.json();
-  }
 
   
   return (
